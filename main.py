498e9f294c9afc1abd9b4fd004fc1092f8d30ca8
Gagandeep Singh <gagan.2492@gmail.com>
2024-08-29 21:39:09 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 from fastapi import FastAPI, HTTPException
 from fastapi.responses import JSONResponse
+from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 from typing import List
 import json
